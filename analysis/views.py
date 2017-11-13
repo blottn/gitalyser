5b943267bf0ec3bfd0ab33681da00d25333b0165
Nicholas Blott <blottn@tcd.ie>
2017-11-13 15:54:13 +0000

    added github api initegration, hid the client secrets though :^)

--- analysis/views.py
+++ analysis/views.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
+from secret import req
 
-# Create your views here.
+import requests
+
+def index(request):
+	return HttpResponse(req())
+
+def callback(request):
+	return HttpResponse('Successfully authenticated!')
