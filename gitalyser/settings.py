02fd2d22c509b4ff999be04a3eca7e1e37862bc4
Nicholas Blott <blottn@tcd.ie>
2017-12-11 12:57:21 +0000

    made sessions secure

--- gitalyser/settings.py
+++ gitalyser/settings.py
@@ -123,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Session stuff
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
+SESSION_COOKIE_HTTPONLY = True
+
